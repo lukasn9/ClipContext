94fb575b8725a9d452581a4c3d6cf7dc619287a6
Lukáš Nagy <luna001111@outlook.com>
2025-11-06 14:49:21 +0100

    Initial Commit

---
+++ ClipContext/ClipContextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipContextApp.swift
+//  ClipContext
+//
+//  Created by Lukáš Nagy on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipContextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
